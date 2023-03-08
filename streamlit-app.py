e582b7dc5466f1d25a67c70059058f708ef927e0
GrahamC <gcsmacc@posteo.net>
2023-03-08 13:05:55 +0100

    updates to streamlit

---
+++ streamlit-app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import Image
+import streamlit.components.v1 as components
+import codecs
+
+st.set_page_config(
+     page_title="The unseen effects of Inflation",
+     page_icon="$$$",
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
