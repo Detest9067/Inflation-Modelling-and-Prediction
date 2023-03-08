d9b0e3d531c5fdf55c319744b2f295bd8c5929f6
GrahamC <gcsmacc@posteo.net>
2023-03-08 13:20:45 +0100

    more updates

--- streamlit-app.py
+++ streamlit-app.py
@@ -13,28 +13,15 @@ st.set_page_config(
      initial_sidebar_state="expanded",
 )
 
-data = go.Choropleth(
-    locations=sorted_df['Country Code'],
-    z=sorted_df['mean_inflation'],
-    text=sorted_df['Country'],
-    colorscale='Viridis',
-    zmin=-5, # set the minimum value of the scale
-    zmax=20, # set the maximum value of the scale
-    colorbar=dict(
-        title='Mean Inflation',
-        tickvals=[-5, 0, 5, 10, 15, 20], # set the tick values of the color bar
-        ticktext=['< -5', '0', '5', '10', '15', '> 20'] # set the tick text of the color bar
-    )
-)
+fig, ax = plt.subplots(figsize=(12,6))
 
-layout = go.Layout(
-    title='Mean Annual Inflation by Country 2008-2022(%)',
-    geo=dict(
-        showframe=False,
-        showcoastlines=False,
-        projection_type='equirectangular'
-    )
-)
+ax.bar(sorted_df.head(10)['Country'], sorted_df.head(10)['mean_inflation'], color='green')
+ax.bar(sorted_df.tail(10)['Country'], sorted_df.tail(10)['mean_inflation'], color='red')
+ax.set_yscale('log')
 
-st.fig = go.Figure(data=data, layout=layout)
-st.fig.show()
+ax.set_xlabel('Country')
+ax.set_ylabel('Mean Inflation (log scale)')
+ax.set_title('Top 10 and Bottom 10 Mean Inflation Rates')
+plt.xticks(rotation=270)
+
+st.plt.show()
