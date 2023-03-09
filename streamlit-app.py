7b5f8fbcfd60f98b6a9b1b63e583a046b2f69b01
GrahamC <gcsmacc@posteo.net>
2023-03-09 10:07:57 +0100

    updates to streamlit app

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from PIL import Image
 import streamlit.components.v1 as components
 import codecs
@@ -12,16 +13,47 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="expanded",
 )
+path = 'data/Global Dataset of Inflation.csv'
+inflation_df = pd.read_csv_file(path)
 
-fig, ax = plt.subplots(figsize=(12,6))
+#code for the overview of inflation
+layout = go.Layout(
+    title='Mean Annual Inflation by Country 2008-2022(%)',
+    geo=dict(
+        showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    )
+)
 
-ax.bar(sorted_df.head(10)['Country'], sorted_df.head(10)['mean_inflation'], color='green')
-ax.bar(sorted_df.tail(10)['Country'], sorted_df.tail(10)['mean_inflation'], color='red')
-ax.set_yscale('log')
+# Define the initial data for the map
+data = go.Choropleth(
+    locations=df['Country Code'],
+    z=df['mean_inflation'],
+    text=df['Country'],
+    colorscale='Viridis',
+    zmin=-5, # set the minimum value of the scale
+    zmax=20, # set the maximum value of the scale
+    colorbar=dict(
+        title='Mean Inflation',
+        tickvals=[-5, 0, 5, 10, 15, 20], # set the tick values of the color bar
+        ticktext=['< -5', '0', '5', '10', '15', '> 20'] # set the tick text of the color bar
+    )
+)
 
-ax.set_xlabel('Country')
-ax.set_ylabel('Mean Inflation (log scale)')
-ax.set_title('Top 10 and Bottom 10 Mean Inflation Rates')
-plt.xticks(rotation=270)
+# Define the slider widget for the years
+year = st.slider("Select a year", 2008, 2022)
 
-st.plt.show()
+# Filter the data based on the selected year
+filtered_data = df[df["Year"] == year]
+
+# Update the data for the map based on the selected year
+data.locations = filtered_data['Country Code']
+data.z = filtered_data['mean_inflation']
+data.text = filtered_data['Country']
+
+# Create the figure using Plotly
+fig = go.Figure(data=data, layout=layout)
+
+# Display the map
+st.plotly_chart(fig)
